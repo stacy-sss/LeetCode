f282adc4a1bc664a88d4bbe122c11d133381d516
stacy-sss <novozhilovanastyusha@yandex.ru>
2026-07-05 20:51:40 +0300

    Time: 0 ms (100%), Space: 7.9 MB (72.63%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n <=2){
+            return n; 
+        }
+        int p1 = 2;
+        int p2 = 1;
+        for(int i = 3; i<=n; i++){
+            int cur = p1 + p2;
+            p2 = p1;
+            p1 = cur;
+        }
+        return p1;
+    }
+};
